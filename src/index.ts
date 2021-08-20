6dc881f5de1f56b071514d8a18b6c466ecac3204
Richard Reyes <rsrprivat@gmail.com>
2021-08-20 21:04:57 +0200

    tuple added

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 
 const reader = new CsvFileReader('football.csv')
+reader.read()
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
+console.log(reader.data[0])
 
 let manUnitedWins = 0
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
