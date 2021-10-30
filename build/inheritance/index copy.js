15f7e9ecd5a1be00f7bc1a747be7f355a550628e
Richard <rsrprivat@gmail.com>
2021-10-30 10:11:30 +0200

    testing composition

--- build/inheritance/index copy.js
+++ build/inheritance/index copy.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
 var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
-var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
-matchReader.load();
+var reader = new MatchReader_1.MatchReader("football.csv");
+reader.read();
+// console.log(reader.data[0])
 var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
     var match = _a[_i];
     if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
--- src/inheritance/index copy.ts
+++ src/inheritance/index copy.ts
@@ -1,14 +1,14 @@
-import { CsvFileReader } from "./CsvFileReader"
 import { MatchReader } from "./MatchReader"
 import { MatchResult } from "./MatchResult"
 
-const csvFileReader = new CsvFileReader("football.csv")
-const matchReader = new MatchReader(csvFileReader)
-matchReader.load()
+const reader = new MatchReader("football.csv")
+reader.read()
+
+// console.log(reader.data[0])
 
 let manUnitedWins = 0
 
-for (let match of matchReader.matches) {
+for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
